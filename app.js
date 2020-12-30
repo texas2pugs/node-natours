ceb896e83225619ec98dbf97881b47ed2ab30f6f
Christopher Inman <christophereinman@gmail.com>
2020-12-30 11:46:07 -0600

    Added first GET req

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from node', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from node', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('Post was successful!');
+// app.post('/', (req, res) => {
+//   res.send('Post was successful!');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 5000;
