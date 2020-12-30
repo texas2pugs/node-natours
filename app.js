41112e1d1d00df3c3549dde2d68486959e2d015e
Christopher Inman <christophereinman@gmail.com>
2020-12-30 12:34:33 -0600

    Get one tour

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Hello from node', app: 'Natours' });
 // });
@@ -24,6 +26,33 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1; // Trick to convert string to number
+  const tour = tours.find((el) => el.id === id);
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// app.post('/app/v1/tours', (req, res) => {
+//   const newId = tours[tours.length - 1].id + 1;
+//   const newTour = Object.assign({ id: newId }, req.body);
+//   tours.push(newTour);
+// });
 
 const port = 5000;
 app.listen(port, () => {
