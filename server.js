5e58086a338ed43ccb7e75790243012082a7765f
Christopher Inman <christophereinman@gmail.com>
2020-12-30 20:55:05 -0600

    Env vars

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 5000;
+console.log(process.env);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
